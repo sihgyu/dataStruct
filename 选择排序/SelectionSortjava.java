78c1520ef8f116d3402bfa5d49ce4242f4ff8356
GuosiHg <1142570567@qq.com>
2018-03-15 20:11:28 +0800

    添加java代码
    选择排序java，c++

---
+++ 选择排序/SelectionSortjava.java
@@ -0,0 +1,29 @@
+public class SelectionSortjava{
+    public static void sort(int[] arr){
+        int n = arr.length;
+        for (int i = 0;i < n;i++){
+            int minIndex = i;
+            for(int j = i+1;j < n;j++)
+                if(arr[j] < arr[minIndex])
+                    minIndex = j;
+                swap(arr,i,minIndex);
+        }
+}
+private static void swap(int[] arr,int i,int j){
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+}
+
+public static void main(String[] args){
+        int[] arr = {10,9,8,7,6,5,4,3,2,1};
+        SelectionSortjava.sort(arr);
+        for (int i = 0;i < arr.length;i++){
+            System.out.print(arr[i]);
+            System.out.print(" ");
+        }
+            System.out.println();
+        }
+
+}
+
